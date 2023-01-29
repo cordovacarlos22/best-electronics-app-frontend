c8d78f0727094c971edf4c1763fc234a8b141a24
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-01-29 09:51:06 -0800

    Update Slider.jsx

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -8,26 +8,26 @@ const Slider = (props) => {
     {
       title:"Laptops and computers",
       desc:"lorem ",
-      url:'https://images.unsplash.com/photo-1674577201145-da3fc3041698?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1588&q=80'
+      url:'https://media.wired.com/photos/5faed077f9e76246dde3355d/master/pass/Gear-Surface-Laptop-Go-SOURCE-Microsoft.jpg'
     },
     {
       title: "Tvs",
       desc: "lorem ",
-      url: 'https://images.unsplash.com/photo-1661961112134-fbce0fdf3d99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80'
+      url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-tvs-good-housekeeping-1657720829.jpg'
     },
     {
       title: "Apple",
       desc: "lorem ",
-      url: 'https://images.unsplash.com/photo-1674788052100-77e2aa1f0e82?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+      url: 'https://images.macrumors.com/t/xrfe0ee20CA_Ce6IyM9VEdjc_6I=/1600x0/article-new/2022/09/apple-beta-lineup-2022.jpg'
     },
     {
       title: "Videos Games",
       desc: "lorem ",
-      url: 'https://images.unsplash.com/photo-1674807309279-6b54689e90d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+      url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gh-index-gamingconsoles-052-print-preview-1659705142.jpg?crop=1.00xw:0.753xh;0,0.0831xh&resize=1200:*'
     }, {
       title: "Major Appliances",
       desc: "lorem ",
-      url: 'https://images.unsplash.com/photo-1674769021687-63263e148cd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80'
+      url: 'https://pisces.bbystatic.com/image2/BestBuy_US/dam/pm-sm-MMT-644589_DER-535b48fa-dd2f-451a-9b58-3d3dff5806be.jpg;maxHeight=350;maxWidth=1000'
     },
   ]
 
@@ -48,17 +48,17 @@ const Slider = (props) => {
   // style = {{ backgroundImage: `url(${slides[currentIndex].url})` }}
 
   return (
-    <div className='slider-container  h-[850px] w-screen m-auto py-16 px-4 relative   bg-cover duration-500  flex  bg-blue-600  justify-items-center items-center  flex-wrap  '>
+    <div className='slider-container  h-screen w-screen m-auto py-16 px-4 relative   bg-cover duration-500  flex  bg-blue-600  justify-items-center items-center  flex-wrap  '>
      
 
       <div 
-        className=' w-full h-full justify-center items-center flex gap-11 bg-white rounded-2xl  group flex-col'>
-        <h1 className='font-extrabold text-[50px] '>Deal Mode Activated</h1>
+        className=' w-2/4 h-2-4 justify-center items-center flex gap-11 bg-white rounded-2xl  group flex-col absolute right-[25%]  p-4 m-4'>
+        <h1 className='font-extrabold text-[50px] text-[#1C4ED8]'>Deal Mode Activated</h1>
         
-        <img className='w-[90px] h-[90px]' src={slides[currentIndex].url} alt='#'></img>
+        <img className='w-3/4 h-w-3/4 rounded-lg' src={slides[currentIndex].url} alt='#'></img>
 
-        <div className='flex flex-col '>
-          <h1 className='font-extrabold font-xl text-2xl'>{slides[currentIndex].title}</h1>
+        <div className='flex flex-col  text-center'>
+          <h1 className='font-extrabold font-xl text-2xl '>{slides[currentIndex].title}</h1>
 
           <p className='font-light text-[] '>{slides[currentIndex].desc}</p></div>
 
