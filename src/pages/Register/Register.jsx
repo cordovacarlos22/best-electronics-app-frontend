6348d8ac5d3ad879fac48736899e9841db52f2c4
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-02-01 08:01:41 -0800

    update on register,login

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,27 +1,82 @@
 import React from 'react'
+import styled from "styled-components";
 
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: white;
+ 
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
 const Register = () => {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='border-2 w-90  flex flex-col bg-blue-700'>
-        <h1 className='text-2xl'>CREATE AN ACCOUNT</h1>
-        <form className='flex flex-col'>
-          
-          <label><input placeholder="name"></input></label>
-          <input  placeholder="last name" />
-          <input  placeholder="username" />
-          <input  placeholder="email" />
-          <input   placeholder="password" />
-          <input   placeholder="confirm password" />
-          <p>
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="name" />
+          <Input placeholder="last name" />
+          <Input placeholder="username" />
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
-          </p>
-          <button>CREATE</button>
-        </form>
-      </div>
-    </div>
-  )
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+    
+  
 }
 
 export default Register
