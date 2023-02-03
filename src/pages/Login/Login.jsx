c06368301063969521123236be147ef190aa493f
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-02-03 15:47:48 -0800

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -61,17 +61,17 @@ const Login = () => {
  
   return (
     <div
-      className='w-screen flex flex-col  justify-center items-center bg-[#EFF2F4]'
+      className='w-screen  flex flex-col  justify-center items-center bg-[#EFF2F4] '
     >
-      <nav className='bg-blue-700 w-screen h-16  flex justify-between items-center p-2'>
-        <div className='logo-container font-bold text-2x1 cursor-pointer flex items-center font-[Poppins] text-white gap-2  text-2xl md:items-center '>
+      <nav className='bg-blue-700 w-screen   flex justify-between items-center p-2'>
+        <div className='logo-container font-bold text-2x1 cursor-pointer flex items-center font-[Poppins] text-white gap-1  text-2xl md:items-center '>
           <Link className='logo-container font-bold text-2x1 cursor-pointer flex items-center font-[Poppins] text-white gap-2  text-2xl md:items-center' as={Link} to='/' alt='logo'>
-            <span className='text-3x1 text-[#FFE000]'><ion-icon name="pricetags-outline"></ion-icon> </span>
+            <span className='text-2x1 text-[#FFE000]'><ion-icon name="pricetags-outline"></ion-icon> </span>
             <h1 className='flex flex-col  text-2xl'>Best Deals <hr /> <span className='text-sm text-[#FFE000]'>Electronics</span></h1>
           </Link>
           <br />
         </div>
-        <div className='  flex items-end items-end'>
+        <div className='  flex items-end'>
           <Link as={Link} to='/'><h2 className=' text-white '>Return to previous page</h2></Link>
         </div>
       </nav>
@@ -83,7 +83,7 @@ const Login = () => {
         >
          
           <h1 className='text-3xl text-extrabold text-blue-600 '> Dest Deals Electronic Sign In</h1>
-          <form className='form flex flex-col gap-2 w-2/4'>
+          <form className='form flex flex-col gap-2 w-3/4 h-2/4 justify-center'>
 
 
             <input
@@ -98,15 +98,16 @@ const Login = () => {
             <button className='bg-blue-600 mb-2 text-white text-xl text-extrabold'>Sign In</button>
           </form>
           <Link><p className='text-blue-800 hover:underline'>Forgot your password?</p></Link>
-          <p className='p-2 flex items-center justify-center w-full'>By continuing you agree to our Terms and Conditions, our Privacy Policy, and the Best Deals <Link as={Link} to="/terms"><span className='text-blue-600 hover:underline'>Terms.</span></Link></p>
+          <p className='p-2  flex items-center justify-center w-full'>By continuing you agree to our Terms and Conditions, our Privacy Policy, and the Best Deals  </p>
+          <Link as={Link} to="/terms"><span className='text-blue-600 hover:underline'>Terms.</span></Link>
           <hr className='w-full'></hr>
-          <h2>Don't have an account? <Link as={Link} to="/UserRegister"><span className='text-blue-600 hover:underline'>Create an account</span></Link></h2>
+          <h2 className='p-2'>Don't have an account? <Link as={Link} to="/UserRegister"><span className='text-blue-600 hover:underline'>Create an account</span></Link></h2>
         </div>
         
      </div>
-      <div className='w-screen h-[200px] bg-blue-600'>
-        <p>
-
+      <div className='w-screen h-[200px] bg-white justify-center items-center flex p-2'>
+        <p >
+          Prices and offers are subject to change. © 2023 Best Deals. All rights reserved. Best Deals, the Best Deals logo, the Tag Design  are trademarks of Best Deals.
         </p>
       </div>
     </div>
